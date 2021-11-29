3e82377c2b8fbb38ad3bb726fa4b2ad8526c9a0c
Seongwoo Park <omegabunh@naver.com>
2021-11-29 12:30:32 +0900

    Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:chatify_app/providers/authentication_provider.dart';
 import 'package:flutter/material.dart';
 
 //Packages
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:provider/provider.dart';
 
 //Services
 import './services/navigation_services.dart';
 
+//Providers
+import './providers/authentication_provider.dart';
+
 //Pages
 import './pages/splash_page.dart';
 import './pages/login_page.dart';
+import './pages/home_page.dart';
 
 void main() {
   runApp(
@@ -26,20 +32,30 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CompanyChat',
-      theme: ThemeData(
-        backgroundColor: Color.fromRGBO(155, 217, 191, 1.0),
-        scaffoldBackgroundColor: Color.fromRGBO(155, 217, 191, 1.0),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(79, 109, 98, 1.0),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (BuildContext _context) {
+            return AuthenticationProvider();
+          },
+        )
+      ],
+      child: MaterialApp(
+        title: 'CompanyChat',
+        theme: ThemeData(
+          backgroundColor: Color.fromRGBO(155, 217, 191, 1.0),
+          scaffoldBackgroundColor: Color.fromRGBO(155, 217, 191, 1.0),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(79, 109, 98, 1.0),
+          ),
         ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {
+          '/login': (BuildContext _context) => LoginPage(),
+          '/home': (BuildContext _context) => HomePage(),
+        },
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext _context) => LoginPage(),
-      },
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,17 @@
 //Packages
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
 
 //Widgets
 import '../widgets/custom_input_fields.dart';
 import '../widgets/rounded_button.dart';
+
+//Providers
+import '../providers/authentication_provider.dart';
+
+//Services
+import '../services/navigation_services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,12 +24,20 @@ class _LoginPageState extends State<LoginPage> {
   late double _deviceHeight;
   late double _deviceWidth;
 
+  late AuthenticationProvider _auth;
+  late NavigationService _navigation;
+
   final _loginFormKey = GlobalKey<FormState>();
+
+  String? _email;
+  String? _password;
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    _navigation = GetIt.instance.get<NavigationService>();
     return _buildUI();
   }
 
@@ -83,13 +99,21 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CustomTextFormField(
-                onSaved: (_value) {},
+                onSaved: (_value) {
+                  setState(() {
+                    _email = _value;
+                  });
+                },
                 regEx:
                     r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`₩{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
                 hintText: 'Email',
                 obscureText: false),
             CustomTextFormField(
-                onSaved: (_value) {},
+                onSaved: (_value) {
+                  setState(() {
+                    _password = _value;
+                  });
+                },
                 regEx: r".{8,}",
                 hintText: 'Password',
                 obscureText: true),
@@ -104,7 +128,12 @@ class _LoginPageState extends State<LoginPage> {
       name: "Login",
       height: _deviceHeight * 0.065,
       width: _deviceWidth * 0.65,
-      onPressed: () {},
+      onPressed: () {
+        if (_loginFormKey.currentState!.validate()) {
+          _loginFormKey.currentState!.save();
+          _auth.loginUsingEmailAndPassword(_email!, _password!);
+        }
+      },
     );
   }
 
