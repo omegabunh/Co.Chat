c000c970270e23a924f8aac207f006941161b398
Seongwoo Park <omegabunh@naver.com>
2021-11-29 17:53:13 +0900

    Register page

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import './providers/authentication_provider.dart';
 //Pages
 import './pages/splash_page.dart';
 import './pages/login_page.dart';
+import './pages/register_page.dart';
 import './pages/home_page.dart';
 
 void main() {
@@ -53,6 +54,7 @@ class MainApp extends StatelessWidget {
         initialRoute: '/login',
         routes: {
           '/login': (BuildContext _context) => LoginPage(),
+          '/register': (BuildContext _context) => RegisterPage(),
           '/home': (BuildContext _context) => HomePage(),
         },
       ),
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,162 @@
+//Packages
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+
+//Services
+import '../services/media_service.dart';
+import '../services/database_service.dart';
+import '../services/cloud_storage_service.dart';
+
+//Widgets
+import '../widgets/custom_input_fields.dart';
+import '../widgets/rounded_button.dart';
+import '../widgets/rounded_image.dart';
+
+//Providers
+import '../providers/authentication_provider.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RegisterPageState();
+  }
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  String? _email;
+  String? _password;
+  String? _name;
+
+  PlatformFile? _profileImage;
+
+  final _registerFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03,
+            vertical: _deviceHeight * 0.02,
+          ),
+          height: _deviceHeight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _profileImageField(),
+              SizedBox(
+                height: _deviceHeight * 0.05,
+              ),
+              _registerForm(),
+              SizedBox(
+                height: _deviceHeight * 0.05,
+              ),
+              _registerButton(),
+              SizedBox(
+                height: _deviceHeight * 0.02,
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImpageFromLbrary().then(
+          (_file) {
+            setState(
+              () {
+                _profileImage = _file;
+              },
+            );
+          },
+        );
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+            key: UniqueKey(),
+            imagePath:
+                "https://w.namu.la/s/071e5e5c31b9a95c91162f6f9024d7c507776d48c05e7e52b4669e6571888a3f70690efc9ecf20da9d955eec2f77684d0ecb58908a8515f82dfcda9226ea85886bc6cb46dd3711f7495a99cd0074a26cfeb0c779c9432bfe0fbb923e7c2d44defb081104ed8c017c15144b5f8871752c",
+            size: _deviceHeight * 0.15,
+          );
+        }
+      }(),
+    );
+  }
+
+  Widget _registerForm() {
+    return Container(
+      height: _deviceHeight * 0.35,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+                onSaved: (_value) {
+                  setState(() {
+                    _name = _value;
+                  });
+                },
+                regEx: r'.{8,}',
+                hintText: "Name",
+                obscureText: false),
+            CustomTextFormField(
+                onSaved: (_value) {
+                  setState(() {
+                    _email = _value;
+                  });
+                },
+                regEx:
+                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`₩{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                hintText: "Email",
+                obscureText: false),
+            CustomTextFormField(
+                onSaved: (_value) {
+                  setState(() {
+                    _password = _value;
+                  });
+                },
+                regEx: r'.{8,}',
+                hintText: "Password",
+                obscureText: true),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return RoundedButton(
+      name: "Register",
+      height: _deviceHeight * 0.065,
+      width: _deviceWidth * 0.65,
+      onPressed: () async {},
+    );
+  }
+}
