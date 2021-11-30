0e36590f79ee51973e01b9cdc334cf70372e1212
Seongwoo Park <omegabunh@naver.com>
2021-11-30 16:37:32 +0900

    QR page

--- lib/pages/qr_page.dart
+++ lib/pages/qr_page.dart
@@ -1,7 +1,15 @@
 //Packages
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+import 'package:timer_builder/timer_builder.dart';
+import 'package:date_format/date_format.dart';
+
+//Providers
+import '../providers/authentication_provider.dart';
 
 class QrPage extends StatefulWidget {
   @override
@@ -11,14 +19,52 @@ class QrPage extends StatefulWidget {
 }
 
 class _QrPageState extends State<QrPage> {
-  @override
+  //qr code time
+  String now = formatDate(DateTime.now(), [hh, ':', nn, ':', ss, ' ', am]);
+
   Widget build(BuildContext context) {
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      //backgroundColor: Colors.blue,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  QrImage(
+                    //data: "박성우 $now",
+                    data: "test",
+                    version: QrVersions.auto,
+                    size: 200,
+                    backgroundColor: Colors.white,
+                    gapless: false,
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+              TimerBuilder.periodic(
+                const Duration(seconds: 1),
+                builder: (context) {
+                  return Text(
+                    formatDate(DateTime.now(), [hh, ':', nn, ':', ss, ' ', am]),
+                    style: const TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
